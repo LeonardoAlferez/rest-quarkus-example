166a9a37dad77bc86f6d7faad8f5447918676fc7
Leonardo Alferez <is705310@iteso.mx>
2022-02-21 13:05:07 -0600

    Third commit. - Swagger API & Microprofile OpenAPI added.

--- src/main/java/org/acme/Movie.java
+++ src/main/java/org/acme/Movie.java
@@ -4,13 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 //@RequiredArgsConstructor
+@Schema(name = "Movie", description = "Movie representation")
 public class Movie {
     private Long id;
+    @Schema(required = true)
     private String title;
 
 }
